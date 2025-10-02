fcf532a6bf39d7b423fb55c9bda5426d80cbb3e3
donoMondes <domondes123@gmail.com>
2025-10-02 20:49:06 +0200

    build try

--- drivers/input/iqs5xx.h
+++ drivers/input/iqs5xx.h
@@ -150,7 +150,7 @@
 #define DEFAULT_ZOOM_INITIAL_DISTANCE 0x32 
 #define DEFAULT_ZOOM_CONSECUTIVE_DISTANCE 0x19
 
-struct iqs5xx_sys_info_1{
+typedef struct iqs5xx_sys_info_1{
     bool tp_movement         :1;
     bool palm_detect         :1;
     bool too_many_fingers    :1;
@@ -160,7 +160,7 @@ struct iqs5xx_sys_info_1{
     uint8_t spare            :2;
 };
 
-struct iqs5xx_sys_info_0{
+typedef struct iqs5xx_sys_info_0{
     uint8_t current_mode     :3;
     bool ati_error           :1;
     bool reati_occured       :1;
@@ -169,12 +169,12 @@ struct iqs5xx_sys_info_0{
     bool show_reset          :1;
 };
 
-struct iqs5xx_sys_info{
+typedef struct iqs5xx_sys_info{
     iqs5xx_sys_info_0 sys_inf0;
     iqs5xx_sys_info_1 sys_inf1;
 };
 
-struct iqs5xx_sys_control_0{
+typedef struct iqs5xx_sys_control_0{
     uint8_t mode_select :3;
     bool    reseed      :1;
     bool    alp_reseed  :1;
@@ -183,13 +183,13 @@ struct iqs5xx_sys_control_0{
     bool    ack_reset   :1;
 };
 
-struct iqs5xx_sys_control_1{
+typedef struct iqs5xx_sys_control_1{
     bool    suspend     :1;
     bool    reset       :1;
     bool    unused      :6; 
 };
 
-struct iqs5xx_sys_control{
+typedef struct iqs5xx_sys_control{
     iqs5xx_sys_control_0 sys_ctrl0;
     iqs5xx_sys_control_1 sys_ctrl1;
 };
