91805d7c6494cb456ee4e4e0238e12c1a8b877d2
donoMondes <domondes123@gmail.com>
2025-12-04 01:07:14 +0100

    modifying iqs5xx driver

--- drivers/input/iqs5xx.c
+++ drivers/input/iqs5xx.c
@@ -18,6 +18,8 @@
 
 LOG_MODULE_REGISTER(iqs5xx, CONFIG_INPUT_LOG_LEVEL);
 
+struct k_mutex iqs5xx_mutex;
+
 static int iqs5xx_read_reg16(const struct device *dev, uint16_t reg, uint16_t *val) {
     const struct iqs5xx_config *config = dev->config;
     uint8_t buf[2];
@@ -65,62 +67,59 @@ static void iqs5xx_button_release_work_handler(struct k_work *work) {
     struct k_work_delayable *dwork = k_work_delayable_from_work(work);
     struct iqs5xx_data *data = CONTAINER_OF(dwork, struct iqs5xx_data, button_release_work);
 
-    // TODO: This loop should only deactivate one button.
-    // Log a warning when that is not the case.
-    for (int i = 0; i < 3; i++) {
-        LOG_INF("Releasing synthetic button");
-        if (data->buttons_pressed & BIT(i)) {
-            input_report_key(data->dev, INPUT_BTN_0 + i, 0, true, K_FOREVER);
-            // Turn off the bit.
-            // NOTE: This is a potential race.
-            data->buttons_pressed &= ~BIT(i);
-        }
+    k_mutex_lock(&iqs5xx_mutex, K_FOREVER);
+    if (data->tap_in_progress) {
+        input_report_key(data->dev, LEFT_BUTTON_CODE, 0, true, K_FOREVER);
+        data->tap_in_progress = false;
     }
+    if (data->two_finger_tap_in_progress) {
+        input_report_key(data->dev, RIGHT_BUTTON_CODE, 0, true, K_FOREVER);
+        data->two_finger_tap_in_progress = false;
+    }
+    k_mutex_unlock(&iqs5xx_mutex);
 }
 
 static void iqs5xx_work_handler(struct k_work *work) {
     struct iqs5xx_data *data = CONTAINER_OF(work, struct iqs5xx_data, work);
     const struct device *dev = data->dev;
     const struct iqs5xx_config *config = dev->config;
-    uint8_t sys_info_0, sys_info_1, gesture_events_0, gesture_events_1, num_fingers;
     int ret;
 
+    uint16_t addr;
+
+    struct iqs5xx_sys_info sys_info;
+    struct iqs5xx_gesture_data gesture_events;
+    struct iqs5xx_touch touch_data;
+    memset(&sys_info,0,sizeof(struct iqs5xx_sys_info));
+    memset(&gesture_events,0,sizeof(struct iqs5xx_gesture_data));
+    memset(&touch_data,0,sizeof(struct iqs5xx_touch));
+
+    addr = TO_LE(IQS5XX_SYSTEM_INFO_0);
+
     // Read system info registers.
-    ret = iqs5xx_read_reg8(dev, IQS5XX_SYSTEM_INFO_0, &sys_info_0);
+    ret = i2c_write_read_dt(&config->i2c, &addr, sizeof(addr), (uint8_t *)&sys_info,2);
     if (ret < 0) {
         LOG_ERR("Failed to read system info 0: %d", ret);
-        goto end_comm;
+        iqs5xx_end_comm_window(dev);
     }
 
-    ret = iqs5xx_read_reg8(dev, IQS5XX_SYSTEM_INFO_1, &sys_info_1);
-    if (ret < 0) {
-        LOG_ERR("Failed to read system info 1: %d", ret);
-        goto end_comm;
-    }
-
-    ret = iqs5xx_read_reg8(dev, IQS5XX_GESTURE_EVENTS_0, &gesture_events_0);
+    addr = TO_LE(IQS5XX_GESTURE_EVENTS_0);
+    ret = i2c_write_read_dt(&config->i2c, &addr, sizeof(addr), (uint8_t *)&gesture_events,2);
     if (ret < 0) {
         LOG_ERR("Failed to read gesture events: %d", ret);
-        goto end_comm;
-    }
-
-    ret = iqs5xx_read_reg8(dev, IQS5XX_GESTURE_EVENTS_1, &gesture_events_1);
-    if (ret < 0) {
-        LOG_ERR("Failed to read gesture events 1: %d", ret);
-        goto end_comm;
+        iqs5xx_end_comm_window(dev);
     }
 
     // Handle reset indication.
-    if (sys_info_0 & IQS5XX_SHOW_RESET) {
+    if (sys_info.sys_info_0.show_reset) {
         LOG_INF("Device reset detected");
         // Acknowledge reset.
         iqs5xx_write_reg8(dev, IQS5XX_SYSTEM_CONTROL_0, IQS5XX_ACK_RESET);
-        goto end_comm;
+        iqs5xx_end_comm_window(dev);
     }
 
-    bool tp_movement = (sys_info_1 & IQS5XX_TP_MOVEMENT) != 0;
-    bool scroll = (gesture_events_1 & IQS5XX_SCROLL) != 0;
-    if (!scroll) {
+    k_mutex_lock(&iqs5xx_mutex, K_FOREVER);
+    if (!gesture_events.gesture_events_0.scroll) {
         // Clear accumulators if we're not actively scrolling.
         data->scroll_x_acc = 0;
         data->scroll_y_acc = 0;
@@ -128,55 +127,61 @@ static void iqs5xx_work_handler(struct k_work *work) {
 
     uint16_t button_code;
     bool button_pressed = false;
-    if (gesture_events_0 & IQS5XX_SINGLE_TAP) {
+    if (gesture_events.gesture_events_0.single_tap
+        && (!gesture_events.gesture_events_1.scroll
+        || !gesture_events.gesture_events_1.zoom)) {
         button_pressed = true;
+        data->tap_in_progress = true;
         button_code = INPUT_BTN_0;
-    } else if (gesture_events_1 & IQS5XX_TWO_FINGER_TAP) {
+    } 
+    else if (gesture_events.gesture_events_1.two_finger_tap
+        && (!gesture_events.gesture_events_1.scroll
+        || !gesture_events.gesture_events_1.zoom)) {
         button_pressed = true;
+        data->two_finger_tap_in_progress = true;
         button_code = INPUT_BTN_1;
     }
 
-    bool hold_became_active = (gesture_events_0 & IQS5XX_PRESS_AND_HOLD) && !data->active_hold;
-    bool hold_released = !(gesture_events_0 & IQS5XX_PRESS_AND_HOLD) && data->active_hold;
+    addr = TO_LE(IQS5XX_REL_X);
 
     int16_t rel_x, rel_y;
-    if (tp_movement || scroll) {
-        ret = iqs5xx_read_reg16(dev, IQS5XX_REL_X, (uint16_t *)&rel_x);
+    if (sys_info.sys_info_1.tp_movement 
+        || gesture_events.gesture_events_0.scroll) {
+        ret = i2c_write_read_dt(&config->i2c, &addr, sizeof(addr), (uint8_t *)&touch_data,4);
         if (ret < 0) {
-            LOG_ERR("Failed to read relative X: %d", ret);
-            goto end_comm;
-        }
-
-        ret = iqs5xx_read_reg16(dev, IQS5XX_REL_Y, (uint16_t *)&rel_y);
-        if (ret < 0) {
-            LOG_ERR("Failed to read relative Y: %d", ret);
-            goto end_comm;
+            LOG_ERR("Failed to read relative touch data: %d", ret);
+            iqs5xx_end_comm_window(dev);
         }
     }
-
+    rel_x = (int16_t)AZOTEQ_IQS5XX_COMBINE_H_L_BYTES(touch_data.rel_x.h, touch_data.rel_x.l);
+    rel_y = (int16_t)AZOTEQ_IQS5XX_COMBINE_H_L_BYTES(touch_data.rel_y.h, touch_data.rel_y.l);
     // Handle movement and gestures.
     //
     // Each one of these branches needs to send the last report it makes as
     // sync to ensure that the input subsystem processes things in order.
-    if (hold_became_active) {
+    if (gesture_events.gesture_events_0.press_and_hold 
+        && !data->active_hold) {
         LOG_INF("Hold became active");
         input_report_key(dev, LEFT_BUTTON_CODE, 1, true, K_FOREVER);
         data->active_hold = true;
-    } else if (hold_released) {
+    } 
+    else if (!gesture_events.gesture_events_0.press_and_hold 
+        && data->active_hold) {
         LOG_INF("Hold became inactive");
         input_report_key(dev, LEFT_BUTTON_CODE, 0, true, K_FOREVER);
         data->active_hold = false;
-    } else if (button_pressed) {
+    } 
+    else if (button_pressed) {
         // Cancel any pending release.
         k_work_cancel_delayable(&data->button_release_work);
 
         // Press the button immediately.
         input_report_key(dev, button_code, 1, true, K_FOREVER);
-        data->buttons_pressed |= BIT(button_code - INPUT_BTN_0);
 
         // Schedule release after 100ms.
         k_work_schedule(&data->button_release_work, K_MSEC(100));
-    } else if (scroll) {
+    } 
+    else if (gesture_events.gesture_events_0.scroll) {
         // TODO: Expose this divisor.
         int16_t scroll_div = 32;
 
@@ -193,7 +198,7 @@ static void iqs5xx_work_handler(struct k_work *work) {
                                 K_FOREVER);
                 data->scroll_x_acc %= scroll_div;
             }
-            goto end_comm;
+            iqs5xx_end_comm_window(dev);
         }
         if (rel_y != 0) {
             if (config->natural_scroll_y) {
@@ -205,24 +210,16 @@ static void iqs5xx_work_handler(struct k_work *work) {
                                  K_FOREVER);
                 data->scroll_y_acc %= scroll_div;
             }
-
-            goto end_comm;
+            iqs5xx_end_comm_window(dev);
         }
-    } else if (tp_movement) {
-        ret = iqs5xx_read_reg8(dev, IQS5XX_NUM_FINGERS, &num_fingers);
-        if (ret < 0) {
-            LOG_ERR("Failed to read number of fingers: %d", ret);
-            goto end_comm;
-        }
-
+    } 
+    else if (sys_info.sys_info_1.tp_movement && !sys_info.sys_info_1.palm_detect) {
         if (rel_x != 0 || rel_y != 0) {
             input_report_rel(dev, INPUT_REL_X, rel_x, false, K_FOREVER);
             input_report_rel(dev, INPUT_REL_Y, rel_y, true, K_FOREVER);
         }
     }
-
-end_comm:
-    // End communication window.
+    k_mutex_unlock(&iqs5xx_mutex);
     iqs5xx_end_comm_window(dev);
 }
 
@@ -237,11 +234,35 @@ static int iqs5xx_setup_device(const struct device *dev) {
     const struct iqs5xx_config *config = dev->config;
     int ret;
 
+
+    // Change resolution
+    ret = iqs5xx_write_reg16(dev, IQS5XX_RESOLUTION_X,config->resolution_x);
+    ret |= iqs5xx_write_reg16(dev, IQS5XX_RESOLUTION_Y,config->resolution_y);
+
+    // Change report rate value
+    ret |= iqs5xx_write_reg16(dev, IQS5XX_REPORT_RATE_ACTIVE_MODE,config->report_rate_active_mode);
+    ret |= iqs5xx_write_reg16(dev, IQS5XX_REPORT_RATE_IDLE_TOUCH_MODE,config->report_rate_active_mode*5);
+    ret |= iqs5xx_write_reg16(dev, IQS5XX_REPORT_RATE_IDLE_MODE,config->report_rate_active_mode*5);
+
     // Enable event mode and trackpad events.
-    ret = iqs5xx_write_reg8(dev, IQS5XX_SYSTEM_CONFIG_1,
+    ret |= iqs5xx_write_reg8(dev, IQS5XX_SYSTEM_CONFIG_1,
                             IQS5XX_EVENT_MODE | IQS5XX_TP_EVENT | IQS5XX_GESTURE_EVENT);
     if (ret < 0) {
         LOG_ERR("Failed to configure event mode: %d", ret);
+        return ret;
+    }
+
+    // Configure the palm reject area size threshold
+    ret = iqs5xx_write_reg8(dev, IQS5XX_PALM_REJECT_THRESHOLD,config->palm_reject_threshold);
+    if (ret < 0) {
+        LOG_ERR("Failed to configure the plam reject threshold: %d", ret);
+        return ret;
+    }
+
+    // Configure the palm reject timeout
+    ret = iqs5xx_write_reg8(dev, IQS5XX_PALM_REJECT_TIMEOUT,config->palm_reject_timeout);
+    if (ret < 0) {
+        LOG_ERR("Failed to configure the palm reject timeout: %d", ret);
         return ret;
     }
 
@@ -279,7 +300,6 @@ static int iqs5xx_setup_device(const struct device *dev) {
         LOG_ERR("Failed to configure single finger gestures: %d", ret);
         return ret;
     }
-
     // Configure the hold time for the press and hold gesture.
     ret = iqs5xx_write_reg16(dev, IQS5XX_HOLD_TIME, config->press_and_hold_time);
     if (ret < 0) {
@@ -290,10 +310,27 @@ static int iqs5xx_setup_device(const struct device *dev) {
     uint8_t two_finger_gestures = 0;
     two_finger_gestures |= config->two_finger_tap ? IQS5XX_TWO_FINGER_TAP : 0;
     two_finger_gestures |= config->scroll ? IQS5XX_SCROLL : 0;
+    two_finger_gestures |= IQS5XX_ZOOM;
     // Configure multi finger gestures.
     ret = iqs5xx_write_reg8(dev, IQS5XX_MULTI_FINGER_GESTURES_CONF, two_finger_gestures);
     if (ret < 0) {
         LOG_ERR("Failed to configure multi finger gestures: %d", ret);
+        return ret;
+    }
+
+    // Configure the hold time for the press and hold gesture.
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_TAP_TIME, DEFAULT_TAP_TIME);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_TAP_DISTANCE, DEFAULT_TAP_DISTANCE);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_HOLD_TIME, DEFAULT_HOLD_TIME);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_SWIPE_INIT_TIME, DEFAULT_SWIPE_INITIAL_TIME);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_SWIPE_INIT_DISTANCE, DEFAULT_SWIPE_INITIAL_DISTANCE);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_SWIPE_CONSEC_TIME, DEFAULT_SWIPE_CONSECUTIVE_TIME);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_SWIPE_CONSEC_DISTANCE, DEFAULT_SWIPE_CONSECUTIVE_DISTANCE);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_SCROLL_INIT_DISTANCE, DEFAULT_SCROLL_INITIAL_DISTANCE);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_ZOOM_INIT_DISTANCE, DEFAULT_ZOOM_INITIAL_DISTANCE);
+    ret |=iqs5xx_write_reg16(dev, IQS5XX_ZOOM_CONSEC_DISTANCE, DEFAULT_ZOOM_CONSECUTIVE_DISTANCE);
+    if (ret < 0) {
+        LOG_ERR("Failed to configure timing gesture settings: %d", ret);
         return ret;
     }
 
@@ -302,6 +339,7 @@ static int iqs5xx_setup_device(const struct device *dev) {
     xy_config |= config->flip_x ? IQS5XX_FLIP_X : 0;
     xy_config |= config->flip_y ? IQS5XX_FLIP_Y : 0;
     xy_config |= config->switch_xy ? IQS5XX_SWITCH_XY_AXIS : 0;
+    xy_config |= config->palm_rejection ? IQS5XX_ALLOW_PALM_REJECT : 0;
     ret = iqs5xx_write_reg8(dev, IQS5XX_XY_CONFIG_0, xy_config);
     if (ret < 0) {
         LOG_ERR("Failed to configure axes: %d", ret);
@@ -309,7 +347,7 @@ static int iqs5xx_setup_device(const struct device *dev) {
     }
 
     // Configure system settings.
-    ret = iqs5xx_write_reg8(dev, IQS5XX_SYSTEM_CONFIG_0, IQS5XX_SETUP_COMPLETE | IQS5XX_WDT);
+    ret = iqs5xx_write_reg8(dev, IQS5XX_SYSTEM_CONFIG_0, IQS5XX_SETUP_COMPLETE | IQS5XX_WDT | IQS5XX_REATI);
     if (ret < 0) {
         LOG_ERR("Failed to configure system: %d", ret);
         return ret;
@@ -330,10 +368,12 @@ static int iqs5xx_init(const struct device *dev) {
     struct iqs5xx_data *data = dev->data;
     int ret;
 
-    if (!i2c_is_ready_dt(&config->i2c)) {
+    if (!i2c_is_ready_dt(&config->i2c)) { 
         LOG_ERR("I2C device not ready");
         return -ENODEV;
     }
+
+    k_mutex_init(&iqs5xx_mutex);
 
     data->dev = dev;
     k_work_init(&data->work, iqs5xx_work_handler);
@@ -417,6 +457,12 @@ static int iqs5xx_init(const struct device *dev) {
         .switch_xy = DT_INST_PROP(n, switch_xy),                                                   \
         .flip_x = DT_INST_PROP(n, flip_x),                                                         \
         .flip_y = DT_INST_PROP(n, flip_y),                                                         \
+        .report_rate_active_mode = DT_INST_PROP_OR(n, report_rate_active_mode, 10),                \
+        .palm_rejection = DT_INST_PROP(n,palm_rejection),                                          \
+        .palm_reject_threshold = DT_INST_PROP_OR(n, palm_reject_threshold, 100),                   \
+        .palm_reject_timeout = DT_INST_PROP_OR(n, palm_reject_timeout, 1),                         \
+        .resolution_x = DT_INST_PROP_OR(n, resolution_x, 3072),                                    \
+        .resolution_y = DT_INST_PROP_OR(n, resolution_y, 2048),                                    \
         .bottom_beta = DT_INST_PROP_OR(n, bottom_beta, 5),                                         \
         .stationary_threshold = DT_INST_PROP_OR(n, stationary_threshold, 5),                       \
     };                                                                                             \
--- drivers/input/iqs5xx.h
+++ drivers/input/iqs5xx.h
@@ -13,15 +13,20 @@
 
 #define IQS5XX_END_COMM_WINDOW 0xEEEE
 
-#define IQS5XX_SYSTEM_CONTROL_0 0x0431
 // System Control 0 bits.
+#define IQS5XX_SYSTEM_CONTROL_0 0x0431
 #define IQS5XX_ACK_RESET BIT(7)
 #define IQS5XX_AUTO_ATI BIT(5)
 #define IQS5XX_ALP_RESEED BIT(4)
 #define IQS5XX_RESEED BIT(3)
 
-#define IQS5XX_SYSTEM_CONFIG_0 0x058E
+// Report rate for the active mode
+#define IQS5XX_REPORT_RATE_ACTIVE_MODE 0x057A // 2 bytes
+#define IQS5XX_REPORT_RATE_IDLE_TOUCH_MODE 0x057C // 2 bytes
+#define IQS5XX_REPORT_RATE_IDLE_MODE 0x057E // 2 bytes
+
 // System Config 0 bits.
+#define IQS5XX_SYSTEM_CONFIG_0 0x058E
 #define IQS5XX_MANUAL_CONTROL BIT(7)
 #define IQS5XX_SETUP_COMPLETE BIT(6)
 #define IQS5XX_WDT BIT(5)
@@ -31,8 +36,8 @@
 #define IQS5XX_SW_INPUT_SELECT BIT(1)
 #define IQS5XX_SW_INPUT BIT(0)
 
-#define IQS5XX_SYSTEM_CONFIG_1 0x058F
 // System Config 1 bits.
+#define IQS5XX_SYSTEM_CONFIG_1 0x058F
 #define IQS5XX_EVENT_MODE BIT(0)
 #define IQS5XX_GESTURE_EVENT BIT(1)
 #define IQS5XX_TP_EVENT BIT(2)
@@ -43,29 +48,35 @@
 #define IQS5XX_PROX_EVENT BIT(7)
 
 // Filter settings register.
-#define IQS5XX_FILTER_SETTINGS 0x0632
 // Filter settings bits.
+#define IQS5XX_FILTER_SETTINGS 0x0632
 #define IQS5XX_IIR_FILTER BIT(0)
 #define IQS5XX_MAV_FILTER BIT(1)
 #define IQS5XX_IIR_SELECT BIT(2)
 #define IQS5XX_ALP_COUNT_FILTER BIT(3)
 
-#define IQS5XX_SYSTEM_INFO_0 0x000F
 // System Info 0 bits.
+#define IQS5XX_SYSTEM_INFO_0 0x000F
 #define IQS5XX_SHOW_RESET BIT(7)
 #define IQS5XX_ALP_REATI_OCCURRED BIT(6)
 #define IQS5XX_ALP_ATI_ERROR BIT(5)
 #define IQS5XX_REATI_OCCURRED BIT(4)
 #define IQS5XX_ATI_ERROR BIT(3)
 
-#define IQS5XX_SYSTEM_INFO_1 0x0010
 // System Info 1 bits.
+#define IQS5XX_SYSTEM_INFO_1 0x0010
 #define IQS5XX_SWITCH_STATE BIT(5)
 #define IQS5XX_SNAP_TOGGLE BIT(4)
 #define IQS5XX_RR_MISSED BIT(3)
 #define IQS5XX_TOO_MANY_FINGERS BIT(2)
 #define IQS5XX_PALM_DETECT BIT(1)
 #define IQS5XX_TP_MOVEMENT BIT(0)
+
+// Palm reject treshold area size
+#define IQS5XX_PALM_REJECT_THRESHOLD 0x066C
+
+// Palm reject disable timeout
+#define IQS5XX_PALM_REJECT_TIMEOUT 0x066D
 
 // These 2 registers have the same bit map.
 // The first one configures the gestures,
@@ -105,11 +116,110 @@
 #define IQS5XX_SCROLL BIT(1)
 #define IQS5XX_ZOOM BIT(2)
 
+// Resolution configuration
+#define IQS5XX_RESOLUTION_X 0x066E
+#define IQS5XX_RESOLUTION_Y 0x0670
+
 // Axes configuration.
 #define IQS5XX_XY_CONFIG_0 0x0669
 #define IQS5XX_FLIP_X BIT(0)
 #define IQS5XX_FLIP_Y BIT(1)
 #define IQS5XX_SWITCH_XY_AXIS BIT(2)
+#define IQS5XX_ALLOW_PALM_REJECT BIT(3)
+
+// default values
+#define DEFAULT_TAP_TIME 0x96
+#define DEFAULT_TAP_DISTANCE 0x19 
+#define DEFAULT_HOLD_TIME 0x12C 
+#define DEFAULT_SWIPE_INITIAL_TIME 0x64 // 0x96 
+#define DEFAULT_SWIPE_INITIAL_DISTANCE 0x12C 
+#define DEFAULT_SWIPE_CONSECUTIVE_TIME 0x0 
+#define DEFAULT_SWIPE_CONSECUTIVE_DISTANCE 0x7D0 
+#define DEFAULT_SCROLL_INITIAL_DISTANCE 0x32 
+#define DEFAULT_ZOOM_INITIAL_DISTANCE 0x32 
+#define DEFAULT_ZOOM_CONSECUTIVE_DISTANCE 0x19
+
+#define AZOTEQ_IQS5XX_COMBINE_H_L_BYTES(h, l) ((int16_t)(h << 8) | l)
+#define AZOTEQ_IQS5XX_SWAP_H_L_BYTES(b) ((uint16_t)((b & 0xff) << 8) | (b >> 8))
+#define TO_LE(addr) (addr << 8) | (addr >> 8)
+
+typedef union {
+    uint8_t data;
+    struct {
+    bool tp_movement         :1;
+    bool palm_detect         :1;
+    bool too_many_fingers    :1;
+    bool report_missed       :1;
+    bool snap_toggle         :1;
+    bool switch_state        :1;
+    uint8_t spare            :2;
+    };
+}iqs5xx_sys_info_1;
+
+typedef union {
+    uint8_t data;
+    struct {
+    uint8_t current_mode     :3;
+    bool ati_error           :1;
+    bool reati_occured       :1;
+    bool alp_ati_error       :1;
+    bool alp_reati_occured   :1;
+    bool show_reset          :1;
+    };
+}iqs5xx_sys_info_0;
+
+typedef union {
+    uint8_t data;
+    struct {
+    bool single_tap          :1;
+    bool press_and_hold      :1;
+    bool swipe_x_m           :1;
+    bool swipe_x_p           :1;
+    bool swipe_y_p           :1;
+    bool swipe_y_m           :1;
+    };
+}iqs5xx_gesture_events_0;
+
+typedef union {
+    uint8_t data;
+    struct {
+    bool two_finger_tap      :1;
+    bool scroll              :1;
+    bool zoom                :1;
+    uint8_t reserved         :5;
+    };
+}iqs5xx_gesture_events_1;
+
+struct iqs5xx_gesture_data {
+    iqs5xx_gesture_events_0 gesture_events_0;
+    iqs5xx_gesture_events_1 gesture_events_1;
+} __packed;
+
+struct iqs5xx_sys_info {
+    iqs5xx_sys_info_0 sys_info_0;
+    iqs5xx_sys_info_1 sys_info_1;
+} __packed;
+
+typedef union {
+    uint8_t data;
+    struct {
+    uint8_t mode_select :3;
+    bool    reseed      :1;
+    bool    alp_reseed  :1;
+    bool    auto_ati    :1;
+    bool    unused      :1;
+    bool    ack_reset   :1;
+    };
+}iqs5xx_sys_control_0;
+
+typedef union {
+    uint8_t data;
+    struct {
+    bool    suspend     :1;
+    bool    reset       :1;
+    uint8_t unused      :6; 
+    };
+}iqs5xx_sys_control_1;
 
 struct iqs5xx_config {
     struct i2c_dt_spec i2c;
@@ -132,6 +242,18 @@ struct iqs5xx_config {
     bool flip_x;
     bool flip_y;
 
+    // Report rate config
+    uint16_t report_rate_active_mode;
+
+    // Palm rejection
+    bool palm_rejection;
+    uint8_t palm_reject_threshold;
+    uint8_t palm_reject_timeout;
+
+    // Resolution
+    uint16_t resolution_x;
+    uint16_t resolution_y;
+
     // Sensitivity. configuration.
     uint8_t bottom_beta;
     uint8_t stationary_threshold;
@@ -145,9 +267,22 @@ struct iqs5xx_data {
     // TODO: Pack flags into a bitfield to save space.
     bool initialized;
     // Flag to indicate if the button was pressed in a previous cycle.
+    bool tap_in_progress;
+    bool two_finger_tap_in_progress;
+    bool zoom_in_progress;
     uint8_t buttons_pressed;
     bool active_hold;
     // Scroll accumulators.
     int16_t scroll_x_acc;
     int16_t scroll_y_acc;
 };
+
+struct iqs5xx_xy{
+    uint8_t h : 8;
+    uint8_t l : 8;
+} __packed;
+
+struct iqs5xx_touch {
+    struct iqs5xx_xy rel_x; //Touch relative position on the x axis
+    struct iqs5xx_xy rel_y; //Touch relative position on the y axis
+} __packed;
